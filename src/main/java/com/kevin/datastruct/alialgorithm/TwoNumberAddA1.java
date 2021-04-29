b447ec5081c164ec4814b9d3a36143544d05a3cb
chenligeng <2447925377@qq.com>
2021-04-29 14:44:41 +0800

    阿里面试算法题 第一题两数相加

---
+++ src/main/java/com/kevin/datastruct/alialgorithm/TwoNumberAddA1.java
@@ -0,0 +1,23 @@
+package com.kevin.datastruct.alialgorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author clg
+ * 阿里算法第一题  两数相加
+ */
+public class TwoNumberAddA1 {
+
+    //暴力解法
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
