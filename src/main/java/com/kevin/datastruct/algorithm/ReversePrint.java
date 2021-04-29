9a637a91ec0d11cda8c6d22b5c40457bde9893f3
chenligeng <2447925377@qq.com>
2021-04-29 10:16:34 +0800

    剑值offer 06题 倒序打印链表

---
+++ src/main/java/com/kevin/datastruct/algorithm/ReversePrint.java
@@ -0,0 +1,35 @@
+package com.kevin.datastruct.algorithm;
+
+import java.util.Stack;
+
+/**
+ * @author clg
+ * 倒叙打印 链表
+ */
+public class ReversePrint {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public int[] reversePrint(ListNode head) {
+      //思路 先加入栈  然后在输出
+      Stack<ListNode> stack = new Stack<>();
+      ListNode pointer = head;
+      while (pointer != null){
+        stack.push(pointer);
+        pointer = pointer.next;
+      }
+      int length = stack.size();
+      int[] res = new int[length];
+      for (int i = 0; i < length; i++) {
+        res[i] = stack.pop().val;
+      }
+      return res;
+    }
+}
