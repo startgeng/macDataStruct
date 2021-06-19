b93969f4e81bb62a5c6c8dc7e6cbb00e2cd71b24
chenligeng <2447925377@qq.com>
2021-06-19 09:20:37 +0800

    剑指offer03 Hash表

---
+++ src/main/java/com/kevin/datastruct/algorithm/FindArrIsRepeatNumber.java
@@ -0,0 +1,43 @@
+package com.kevin.datastruct.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author chenligeng
+ * 找到数组是重复的元素
+ */
+public class FindArrIsRepeatNumber {
+
+    public int findRepeatNumber(int[] nums) {
+        Map<Integer,Integer> map = new ConcurrentHashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }else {
+                map.put(num,1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer value = entry.getValue();
+            if (value < 2){
+                map.remove(entry.getKey());
+            }
+        }
+        while (!map.isEmpty()){
+            Set<Integer> keys = map.keySet();
+            for (Integer key : keys) {
+                Integer removeNumber = map.remove(key);
+                return key;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 3, 1, 0, 2, 5, 3};
+//        System.out.println(findRepeatNumber(arr));
+    }
+}
