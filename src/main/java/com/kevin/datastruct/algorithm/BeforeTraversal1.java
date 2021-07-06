5486bdd7895645360d30f73dc4a76400e14545b2
chenligeng <2447925377@qq.com>
2021-07-06 17:15:38 +0800

    二叉树的非递归前序遍历

---
+++ src/main/java/com/kevin/datastruct/algorithm/BeforeTraversal1.java
@@ -0,0 +1,33 @@
+package com.kevin.datastruct.algorithm;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author chenligeng
+ * 前序遍历二叉树方法2
+ */
+public class BeforeTraversal1 {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = Lists.newArrayList();
+        if (root == null){
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode t1 = stack.pop();
+            if (t1.left!=null){
+                stack.push(t1.left);
+            }
+            if (t1.right!=null){
+                stack.push(t1.right);
+            }
+            list.add(t1.val);
+        }
+        return list;
+    }
+}
